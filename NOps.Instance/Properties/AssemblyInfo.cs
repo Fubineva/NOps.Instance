a5922aa88b48a8b68a27f462d107f60d54a9fe62
Joppe <joppe@fubineva.nl>
2015-06-15 11:19:00 +0200

    Description missing

--- NOps.Instance/Properties/AssemblyInfo.cs
+++ NOps.Instance/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("NOps.Instance")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Tooling to support multiple instances of an application install and their configuration.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Fubineva")]
 [assembly: AssemblyProduct("NOps.Instance")]
